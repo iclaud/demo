7e873e3616a554e819dca0785ef13c5a32a248e3
unknown <Claud@.(none)>
2013-05-13 01:45:29 +0800

    add file

--- demo.py
+++ demo.py
@@ -1,1 +1,12 @@
-print 'hello py!'
+#Lesson 1: Hello
+print('hello py!');
+
+#Lesson 2:Variants
+demoa = 10;
+print(type(demoa));
+
+#Lesson 3:List
+demoa = (0,1,2);
+demob = [0,1,2];
+
+print(type(demoa), "-", type(demob));
